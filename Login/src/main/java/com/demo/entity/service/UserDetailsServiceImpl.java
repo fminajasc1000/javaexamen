801a82a262aa904eb0b31fed74c0de4f9add81f8
franc <franc@192.168.1.64>
2021-05-09 15:54:55 -0500

    visor

--- Login/src/main/java/com/demo/entity/service/UserDetailsServiceImpl.java
+++ Login/src/main/java/com/demo/entity/service/UserDetailsServiceImpl.java
@@ -1,4 +1,5 @@
 package com.demo.entity.service;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,27 +22,35 @@ import com.demo.repository.UserRepository;
  * 
  * */
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService{
+public class UserDetailsServiceImpl implements UserDetailsService {
 	@Autowired
-    UserRepository userRepository;
-	
-    @Override
-     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-    //Buscar el usuario con el repositorio y si no existe lanzar una exepcion
-    com.demo.entity.User appUser = 
-                 userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("No existe usuario"));
-		
-    //Mapear nuestra lista de Authority con la de spring security 
-    List grantList = new ArrayList();
-    for (Authority authority: appUser.getAuthority()) {
-        // ROLE_USER, ROLE_ADMIN,..
-        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(authority.getAuthority());
-            grantList.add(grantedAuthority);
-    }
-		
-    //Crear El objeto UserDetails que va a ir en sesion y retornarlo.
-    UserDetails user = (UserDetails) new User(appUser.getUsername(), appUser.getPassword(), grantList);
-         return user;
-    }
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		/*
+		 * Buscar el usuario con el repositorio y si no existe lanzar una exepcion
+		 * 
+		 */
+
+		com.demo.entity.User appUser = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("No existe usuario"));
+
+		/*
+		 * Mapear nuestra lista de Authority con la de spring security
+		 * 
+		 */
+
+		List grantList = new ArrayList();
+		for (Authority authority : appUser.getAuthority()) {
+			// ROLE_USER, ROLE_ADMIN,..
+			GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(authority.getAuthority());
+			grantList.add(grantedAuthority);
+		}
+
+		// Crear El objeto UserDetails que va a ir en sesion y retornarlo.
+		UserDetails user = (UserDetails) new User(appUser.getUsername(), appUser.getPassword(), grantList);
+		return user;
+	}
 }
