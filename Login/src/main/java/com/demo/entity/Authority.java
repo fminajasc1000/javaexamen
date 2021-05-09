76996c5cc3ef6327ddec0a2e7686a7d3889a433a
franc <franc@192.168.1.64>
2021-05-09 15:51:47 -0500

    Comentarios

--- Login/src/main/java/com/demo/entity/Authority.java
+++ Login/src/main/java/com/demo/entity/Authority.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 /*
- * Clase para la definicion de las tablas en donde se almaceneran las contraseñas
+ * Clase para la definicion de las tablas en donde se almaceneran los tipos de usuarios
  * asi como del cifrado
  * @Version 1.0 08/05/2021
  * @author Francisco Minajas
@@ -14,6 +14,11 @@ import javax.persistence.Id;
  * */
 @Entity
 public class Authority {
+	/*
+	 * Esta tabla almacena los tipos de usuario
+	 * USER-->
+	 * ADMIN-->
+	 * */
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
--- Login/src/main/java/com/demo/util/Passgenerator.java
+++ Login/src/main/java/com/demo/util/Passgenerator.java
@@ -1,11 +1,19 @@
 package com.demo.util;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+/*
+ * Clase para generar el cifrado de las contraseñas
+ * @Version 1.0 08/05/2021
+ * @author Francisco Minajas
+ * 
+ * */
 public class Passgenerator {
 	public static void main(String[] args) {
+		/*
+		 * El String que mandamos al metodo encode es el password que queremos cifrar.
+		 * 
+		 * */
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
-        //El String que mandamos al metodo encode es el password que queremos encriptar.
 		System.out.println(bCryptPasswordEncoder.encode("1234"));
 	}
 }
